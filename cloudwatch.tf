f5fdbdec2061cc96b018deb075e8f8fe1c4e7712
jibin <perseverancejbt@hotmail.com>
2025-10-05 16:31:42 +0800

    Terraform code for Assignment2.16 with cloudwatch Infocount alarm

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,7 +2,7 @@
 # CloudWatch Log Group (for Lambda)
 # ------------------------------
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.http_api_lambda.function_name}"
+  name              = "/moviedb-api/${aws_lambda_function.http_api_lambda.function_name}"
   retention_in_days = 14
 }
 
@@ -16,11 +16,26 @@ resource "aws_cloudwatch_log_metric_filter" "error_metric_filter" {
 
   metric_transformation {
     name      = "${local.name_prefix}-LambdaErrorCount"
-    namespace = "${local.name_prefix}-LambdaMetrics"
+    namespace  = "/moviedb-api/${local.name_prefix}"
     value     = "1"
   }
 }
 
+# ------------------------------
+# Metric Filter for 'INFO' logs
+# ------------------------------
+resource "aws_cloudwatch_log_metric_filter" "info_metric_filter" {
+  name           = "${local.name_prefix}-lambda-info-count"
+  log_group_name = aws_cloudwatch_log_group.lambda_log_group.name
+  pattern        = "[INFO]"
+
+  metric_transformation {
+    name      = "${local.name_prefix}-LambdaInfoCount"
+    namespace  = "/moviedb-api/${local.name_prefix}"
+    value      = "1"
+    unit       = "None"
+  }
+}
 
 
 # ------------------------------
@@ -40,6 +55,24 @@ resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
 
   alarm_actions = [aws_sns_topic.lambda_alarm_topic.arn]
 }
+
+resource "aws_cloudwatch_metric_alarm" "lambda_info_alarm" {
+  alarm_name          = "${local.name_prefix}-LambdaInfoAlarm"
+  alarm_description   = "Alarm when Lambda logs contain '[INFO]'"
+  namespace           = aws_cloudwatch_log_metric_filter.info_metric_filter.metric_transformation[0].namespace
+  metric_name         = aws_cloudwatch_log_metric_filter.info_metric_filter.metric_transformation[0].name
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = 10
+  evaluation_periods  = 1
+  period              = 60
+  unit                = "Count"
+  statistic           = "Sum"
+  treat_missing_data  = "notBreaching"
+
+  alarm_actions = [aws_sns_topic.lambda_alarm_topic.arn]
+}
+
+
 
 # ------------------------------
 # SNS Topic for Alarm Notifications
